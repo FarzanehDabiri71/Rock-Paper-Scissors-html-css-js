ea734a05be6841516d08c21cd7f51149bf66e500
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-13 22:36:24 +0330

    Persistant results

--- scripts/rsp.js
+++ scripts/rsp.js
@@ -15,28 +15,40 @@ function getComputerChoice() {
 }
 
 // Game results
-let gameResult = {
-  WIN: "You Win.",
-  LOSE: "YOu Lose.",
-  TIE: "It's a Tie.",
+let gameResult = JSON.parse(localStorage.getItem("gameResult")) || {
+  WIN: 0,
+  LOSE: 0,
+  TIE: 0,
 };
 
+function updateResultUI() {
+  document.querySelector(".result").innerHTML = `
+    
+<p>Wins: ${gameResult.WIN}, 
+Losses: ${gameResult.LOSE}, 
+Ties: ${gameResult.TIE}</p>
+`;
+}
 // Function to determine the winner
 function determineWinner(userSelection) {
   const computerSelection = getComputerChoice();
-  if (computerSelection === userSelection) return gameResult.TIE;
-  if (
+  if (computerSelection === userSelection) {
+    gameResult.TIE++;
+  } else if (
     (computerSelection === "rock" && userSelection === "paper") ||
     (computerSelection === "paper" && userSelection === "scissors") ||
     (computerSelection === "scissors" && userSelection === "rock")
   ) {
-    return gameResult.WIN;
+    gameResult.WIN++;
+  } else {
+    gameResult.LOSE++;
   }
-  return gameResult.LOSE;
+  localStorage.setItem("gameResult", JSON.stringify(gameResult));
 }
 
 // Function to display the result
 function playGame(userSelection) {
-  const gameResult = determineWinner(userSelection);
-  document.querySelector(".result").innerHTML = gameResult;
+  determineWinner(userSelection);
+  updateResultUI();
 }
+updateResultUI();
