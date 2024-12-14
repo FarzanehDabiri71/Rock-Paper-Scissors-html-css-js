1e47e2322eb7b998246cd67ac0f9c5f501ffbd0c
FarzanehDabiri71 <farzanehdabiri71@yahoo.com>
2024-12-14 17:52:16 +0330

    display user and computer selections

--- scripts/rsp.js
+++ scripts/rsp.js
@@ -32,19 +32,28 @@ function updateResultUI() {
 }
 // Function to determine the winner
 function determineWinner(userSelection) {
+  let result = "";
   const computerSelection = getComputerChoice();
   if (computerSelection === userSelection) {
     gameResult.TIE++;
+    result = "Tie.";
   } else if (
     (computerSelection === "rock" && userSelection === "paper") ||
     (computerSelection === "paper" && userSelection === "scissors") ||
     (computerSelection === "scissors" && userSelection === "rock")
   ) {
     gameResult.WIN++;
+    result = "You win.";
   } else {
     gameResult.LOSE++;
+    result = "You lose.";
   }
   localStorage.setItem("gameResult", JSON.stringify(gameResult));
+  document.querySelector(".results").innerHTML = result;
+  document.querySelector(".moves").innerHTML = `You
+  <img src="images/${userSelection}-emoji.png" class="move-icon">
+  <img src="images/${computerSelection}-emoji.png" class="move-icon">
+  Computer`;
 }
 
 // Function to display the result
@@ -63,10 +72,13 @@ function resetGame() {
       TIE: 0,
     })
   );
+  document.querySelector(".moves").innerHTML = "";
+  document.querySelector(".results").innerHTML = "";
   gameResult = {
     WIN: 0,
     LOSE: 0,
     TIE: 0,
   };
+
   updateResultUI();
 }
